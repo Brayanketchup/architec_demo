b0836d0d4659dfff87dd655e7ae228a4614659b2
Brayanketchup <112900383+Brayanketchup@users.noreply.github.com>
2025-05-09 14:29:45 -0400

    add vercel file

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
         <Route path='/project/:id' element={<Project />} />
